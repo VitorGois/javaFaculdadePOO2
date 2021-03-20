dcd7653773d951d8849a57e4134b2d2448bd59e1
VitorGois <vitorcgois777@gmail.com>
2021-03-20 20:33:00 -0300

    Created functionality to create, update and remove clients from the database

--- src/main/java/com/facens/system/controllers/ClientController.java
+++ src/main/java/com/facens/system/controllers/ClientController.java
@@ -1,15 +1,24 @@
 package com.facens.system.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import com.facens.system.dtos.ClientDTO;
+import com.facens.system.dtos.ClientInsertDTO;
+import com.facens.system.dtos.ClientUpdateDTO;
 import com.facens.system.services.ClientService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/clients")
@@ -19,11 +28,39 @@ public class ClientController {
     private ClientService service;
 
     @GetMapping()
-    public ResponseEntity<List<ClientDTO>> getClient() {
+    public ResponseEntity<List<ClientDTO>> getClients() {
         
         List<ClientDTO> list = this.service.getClients();
 
         return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientDTO> getClientById(@PathVariable Long id) {
+        ClientDTO dto = this.service.getClientByIdDto(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping()
+    public ResponseEntity<ClientDTO> insertClient(@RequestBody ClientInsertDTO insertDto) {
+        ClientDTO dto = this.service.insertClient(insertDto);
+        
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteClient(@PathVariable Long id) {
+        this.service.deleteClient(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ClientDTO> updateClient(@RequestBody ClientUpdateDTO updateDto, @PathVariable Long id) {
+        ClientDTO dto = this.service.updateClient(id, updateDto);
+        
+        return ResponseEntity.ok().body(dto); 
     }
 
 }
---
+++ src/main/java/com/facens/system/dtos/ClientUpdateDTO.java
@@ -0,0 +1,15 @@
+package com.facens.system.dtos;
+
+public class ClientUpdateDTO {
+    
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
